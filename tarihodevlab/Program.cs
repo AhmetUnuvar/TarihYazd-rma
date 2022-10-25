e749e85dce85d5483d8f8d8ef01f4cc31be64d5c
AhmetUnuvar <95508776+AhmetUnuvar@users.noreply.github.com>
2022-10-26 02:26:15 +0300

    Tarih Yazdırma

---
+++ tarihodevlab/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarihodevlab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TarihFormat();
+            Console.ReadLine();
+        }
+        public static void TarihFormat()
+        {
+
+            DateTime simdi = DateTime.Now;
+            Console.WriteLine(simdi.ToLongDateString());
+            Console.ReadKey();
+            
+        }
+    }
+}
